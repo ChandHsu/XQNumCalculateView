90b97c0e9a0a5ad7466c930ec139a57c6c2ce510
qianggeProgramer <xuqiang.0205@qq.com>
2016-03-10 13:25:28 +0800

    基本功能实现

--- XQNumCalculateView/ViewController.m
+++ XQNumCalculateView/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "XQNumCalculateView.h"
+#import "UIColor+Random.h"
 
 @interface ViewController ()
 
@@ -16,12 +18,46 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+    
+    
+    XQNumCalculateView *view1 = [[XQNumCalculateView alloc] initWithFrame:CGRectMake(100, 100, 100, 30)];
+    view1.numViewBorderColor = [UIColor randomColor];
+    [self.view addSubview:view1];
+    
+    view1.changeBlock = ^(int result){
+        NSLog(@"%d",result);
+    };
+    
+    
+    view1.maxNum = 10;
+    //    view1.minNum = 0;
+    //    view1.unitNum = 2;
+    //    view1.startNum = 3;
+    view1.calBtnHighTextColor = [UIColor randomColor];
+    view1.calBtnTextColor = [UIColor randomColor];
+    view1.calBtnBgColor = [UIColor randomColor];
+    view1.calBtnHighBgColor = [UIColor randomColor];
+    
+    
+    XQNumCalculateView *view2 = [[XQNumCalculateView alloc] initWithFrame:CGRectMake(100, 140, 140, 30)];
+    view2.numViewBorderColor = [UIColor randomColor];
+    view2.type = XQNumCalculateViewTypeBorderEvery;
+    [self.view addSubview:view2];
+    view2.calBtnHighTextColor = [UIColor randomColor];
+    view2.calBtnTextColor = [UIColor randomColor];
+    view2.calBtnBgColor = [UIColor randomColor];
+    view2.calBtnHighBgColor = [UIColor randomColor];
+    
+    XQNumCalculateView *view3 = [[XQNumCalculateView alloc] initWithFrame:CGRectMake(100, 180, 30, 100)];
+    view3.numViewBorderColor = [UIColor randomColor];
+    [self.view addSubview:view3];
+    
+    XQNumCalculateView *view4 = [[XQNumCalculateView alloc] initWithFrame:CGRectMake(170, 180, 30, 150)];
+    view4.numViewBorderColor = [UIColor randomColor];
+    [self.view addSubview:view4];
+    view4.type = XQNumCalculateViewTypeBorderEvery;
+    
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
