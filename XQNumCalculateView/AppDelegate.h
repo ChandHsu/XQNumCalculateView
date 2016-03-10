8be19732e502f3ae605b48ec954d7c715c2e973e
qianggeProgramer <xuqiang.0205@qq.com>
2016-03-10 13:22:53 +0800

    Initial Commit

---
+++ XQNumCalculateView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XQNumCalculateView
+//
+//  Created by Apple on 16/3/10.
+//  Copyright © 2016年 SJLX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XQNumCalculateView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XQNumCalculateView
+//
+//  Created by Apple on 16/3/10.
+//  Copyright © 2016年 SJLX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
